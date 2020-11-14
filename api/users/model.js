4090e7898495dc57e98ef1a5622d62e2a19b647b
hobbica98 <liviu.calita98@gmail.com>
2020-11-14 19:47:44 +0100

    fix view

--- api/users/model.js
+++ api/users/model.js
@@ -55,7 +55,7 @@ export const schema = {
   }
 }
 export const view = function (user) {
-  return _.remove(user, 'password')
+  return _.omit(user, 'password')
 }
 
 export const data = [{ email: 'admin@admin.com', password: '$2y$04$a8n0ViY73hgwozUB2M.bJuXR.nJOWjSYeOA.pkB9c5YEM9qwIS3nW', id: '1371b9e9-364e-4df3-b99f-9141671007a8', role: 'admin' }]
