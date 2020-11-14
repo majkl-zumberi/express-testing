f2dc8163386df3f8c8368ccdc203b22c2672f267
hobbica98 <liviu.calita98@gmail.com>
2020-11-14 21:38:47 +0100

    moved to depencies

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-require('babel-core/register')
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import api from './api'
+import { errorHandler as queryErrorHandler } from 'querymen'
+import { errorHandler as bodyErrorHandler } from 'bodymen'
 
-exports = module.exports = require('./app')
+const index = express()
+const genericErrorHandler = (err, req, res, next) => {
+  if (req.querymen && req.querymen.error) {
+    res.status(400).json({ error: req.querymen.error })
+  } else if (req.bodymen && req.bodymen.error) {
+    res.status(400).json({ error: req.bodymen.error })
+  } else if (err.errors) {
+    res.status(400).json({
+      error: {
+        valid: false,
+        param: Object.keys(err.errors).join(','),
+        message: err.message
+      }
+    })
+  } else {
+    res.status(400).json({ error: err.message ? err.message : `Undefined error: ${err}` })
+  }
+}
+index.use(bodyParser.json())
+index.use(api)
+index.use(cors())
+index.use(queryErrorHandler())
+index.use(bodyErrorHandler())
+index.use(genericErrorHandler)
+index.listen(process.env.PORT || 3000, () => {
+  console.log('Server started!')
+})
